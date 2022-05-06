cdc5754e84eedc489f864bc713f470ddffa3e02a
Khemraj Prajapati <kumarkhemraj614@gmail.com>
2022-05-06 09:56:01 +0530

    console

--- duplicate.js
+++ duplicate.js
@@ -2,6 +2,7 @@
 //find duplicates
 const arry = [1, 2, 1, 3, 4, 3, 5]
 const findDuplicates = arry.filter((item, index) => arry.indexOf(item) !== index)
-
+console.log('findDuplicates', findDuplicates)
 //remove duplicate
 const removeDuplicates = arry.filter((item, index) => arry.indexOf(item) == index)
+console.log('removeDuplicates', removeDuplicates)
