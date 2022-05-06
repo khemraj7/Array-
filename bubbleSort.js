c8e9f87d5982ae46cfe4cb34f87b1345ce5d3b29
Khemraj Prajapati <kumarkhemraj614@gmail.com>
2022-05-06 11:37:55 +0530

    linear sor and bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,14 @@
+//bubble sort
+//sort in ascending order
+let array = [6, 5, 3, 1, 8, 7, 2, 4]
+var temp
+for (let i = 0; i < array.length - 1; i++) {
+  for (let j = 0; j < array.length - i - 1; j++) {
+    if (array[j] > array[j + 1]) {
+      temp = array[j]
+      array[j] = array[j + 1]
+      array[j + 1] = temp
+    }
+  }
+}
+console.log(array)
---
+++ linearSort.js
@@ -0,0 +1,4 @@
+const array = [3, 4, 2, 7, 1]
+
+//sort using sort method
+console.log(array.sort())
