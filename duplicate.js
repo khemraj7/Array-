5cd3f9b1ac6a05d262912b8e48b8c685719956a8
Khemraj Prajapati <kumarkhemraj614@gmail.com>
2022-05-06 09:53:26 +0530

    find and remove duplicate array

---
+++ duplicate.js
@@ -0,0 +1,7 @@
+//by using filter
+//find duplicates
+const arry = [1, 2, 1, 3, 4, 3, 5]
+const findDuplicates = arry.filter((item, index) => arry.indexOf(item) !== index)
+
+//remove duplicate
+const removeDuplicates = arry.filter((item, index) => arry.indexOf(item) == index)
