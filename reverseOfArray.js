c9c372101a545ece6278c493ba18b3cd8e7c38c2
Khemraj Prajapati <54818964+khemraj7@users.noreply.github.com>
2023-03-27 22:09:10 +0530

    reverse with recursion

--- reverseOfArray.js
+++ reverseOfArray.js
@@ -7,3 +7,22 @@ for (let i = 0; i < array.length / 2; i++) {
 }
 
 console.log('Reverse Array', array)
+
+// reverse with recursion
+let data = [1,2,3,4,5,6,7,8,9]
+let temp;
+function reverse(array, start, end){
+    // console.log(array)
+    if(start<=end){
+    temp = array[start]
+    array[start] = array[end]
+     array[end] = temp
+     reverse(data, start+1, end-1)
+        
+    }
+    
+     return array
+    
+}
+
+console.log(reverse(data, 0, data.length-1))
