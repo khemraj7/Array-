cf9bace8475a9d97e2ba04b9de3bf0154b806a6e
Khemraj Prajapati <54818964+khemraj7@users.noreply.github.com>
2024-05-18 11:50:31 +0530

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -2,35 +2,28 @@ let array = [6, 5, 3, 1, 8, 7, 2, 4]
 let find = 7
 let left = 0
 let right = array.length - 1
-const result = await binarySearch(array, left, right, find)
+
+
+const result =  binarySearch(array, left, right, find)
 if (result == -1) {
   console.log('Not found')
 } else {
   console.log('index of element in array', result, array[result])
 }
 
-binarySearch((arr, left, right, find) => {
-  if (right >= left) {
-    let mid = left + Math.floor((right - left) / 2)
-    // console.log(mid)
-    // If the element is present at the middle
-    if (arr[mid] == find) {
+function binarySearch(arr, left, right, find){
+    if(right >= left){
+       let mid = left + Math.floor((right - left) / 2)
+      
+     if (arr[mid] == find) {
       return mid
+    }  else if (arr[mid] > find) {
+      return binarySearch(arr, left, mid - 1, find)
+    }else{
+        return binarySearch(arr, mid + 1, right, find) 
     }
 
-    // If element is smaller than mid, then
-    // it can only be present in left subarray
-    else if (arr[mid] > find) {
-      return binarySearch(arr, left, mid - 1, find)
+    }else{
+        return -1
     }
-
-    // Else the element can only be present
-    // it can only be present in right subarray
-    else {
-      return binarySearch(arr, left, mid - 1, find)
-    }
-  } else {
-    // We reach here when element is not present in array
-    return -1
-  }
-})
+}
