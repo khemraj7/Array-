56eb5c74df621f938cf5f5a14c7c1a1435854293
Khemraj Prajapati <54818964+khemraj7@users.noreply.github.com>
2024-06-30 14:25:41 +0530

    Create selectionSort.js

---
+++ selectionSort.js
@@ -0,0 +1,18 @@
+function slectionSort(arr){
+    for(let i =0; i < arr.length; i++){
+        
+        let lowest = i
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[j] < arr[lowest]){
+               lowest =j 
+            }
+        }
+       let temp = arr[i]
+                arr[i] = arr[lowest]
+                arr[lowest] = temp
+                
+    }
+    return arr
+}
+
+const result = slectionSort([34,22,10,19,17])
